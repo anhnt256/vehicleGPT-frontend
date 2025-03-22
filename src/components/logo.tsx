6d3a2df0f6f073858cd3145d5b6e5f3bfb34ad15
anhnt <anhnt@cloudverse.ai>
2025-03-23 01:57:13 +0700

    chore: fix logo and remove unuse code

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,11 @@
 export const Logo = () => {
   return (
     <a href="/" className="block">
-      <div className="hover:opacity-75 transition items-center gap-x-2 flex md:flex">
-        <img src="/src/assets/logo.svg" alt="Logo" className="w-auto h-8" />
+      <div className="hover:opacity-75 transition items-center flex">
+        <span className="text-xl sm:text-2xl font-bold">
+          <span className="text-orange-500">Super</span>
+          <span className="text-blue-600 ml-1 font-mono">Todo</span>
+        </span>
       </div>
     </a>
   );
